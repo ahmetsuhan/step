6ca17ddaf446b2171f11f4297ae8def834ddb810
uurtech <girisiyorum@gmail.com>
2021-02-21 12:50:51 +0300

    added sql

---
+++ grammer.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `userinfo` (
+                            `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+                            `alias` VARCHAR(255) NULL,
+                            `command` VARCHAR(255) NULL,
+                            `key_path` VARCHAR(255) NULL,
+);
